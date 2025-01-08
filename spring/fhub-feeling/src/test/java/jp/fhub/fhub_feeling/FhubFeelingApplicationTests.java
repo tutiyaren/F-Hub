13d940177f0c4ab85e30d768cf4e8d1543fb606b
ren <r999b66k222@outlook.jp>
2025-01-08 16:43:55 +0900

    feat: spring initalizrで作成したプロジェクトを当ディレクトリに移行

---
+++ spring/fhub-feeling/src/test/java/jp/fhub/fhub_feeling/FhubFeelingApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.fhub.fhub_feeling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhubFeelingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
